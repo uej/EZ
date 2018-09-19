d557ca53539cf04637d999df6cdf58ab3858163e
uej <534883041@qq.com>
2018-09-19 20:05:01 +0800

    ss

--- app/manage/model/Company.php
+++ app/manage/model/Company.php
@@ -24,6 +24,18 @@ class Company extends Model {
         $data   = $this->create($data);
         $data['createTime'] = time();
         
+        if (empty($data['name'])) {
+            $this->error    = '商户名称不能为空';
+            return FALSE;
+        }
+        if (empty($data['contact'])) {
+            $this->error    = '商户联系人不能为空';
+            return FALSE;
+        }
+        if (empty($data['phone'])) {
+            $this->error    = '商户联系电话不能为空';
+            return FALSE;
+        }
         
     }
 }
